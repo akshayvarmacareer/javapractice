82fc8bddf5b37e36f0eeef34607079978bd97813
akelzi <40731470+akelzi@users.noreply.github.com>
2023-08-12 16:02:08 +0530

    Added Auther in java file

--- javapractice/src/com/akshay/javapractice/App.java
+++ javapractice/src/com/akshay/javapractice/App.java
@@ -1,5 +1,9 @@
 package com.akshay.javapractice;
 
+/**
+ * @auther Akshay
+ * This project only for practicing the java code
+ */
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
