fdb04b84051e51ab320998f968a1d82f6f7d499a
akelzi <40731470+akelzi@users.noreply.github.com>
2023-08-14 20:17:42 +0530

    Two Sum Problem have been solved

--- javapractice/src/com/akshay/javapractice/App.java
+++ javapractice/src/com/akshay/javapractice/App.java
@@ -5,7 +5,32 @@ package com.akshay.javapractice;
  * This project only for practicing the java code
  */
 public class App {
+    /**
+     * Given an array of size N and integer 'target'. Find the indices(i,j) of two
+     * numbers such that their sum is equal to target. (i != j )
+     * You can assume that their will be exactly one solution.
+     * <p>
+     * Sample input
+     * N = 5
+     * arr = {7,6,3,8,2}
+     * target = 14
+     * Sample output
+     * 1 , 3
+     */
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        int target = 14;
+        int[] arr = new int[]{7, 6, 3, 8, 2};
+        int[] indices = twoSum(target, arr);
+    }
+
+    public static int[] twoSum(int target, int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] == target - arr[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return null;
     }
 }
